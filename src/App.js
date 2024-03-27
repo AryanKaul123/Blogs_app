c9f4c604bb8ace3c60fa15f76a7255141c6384bb
unknown <kaularyan63@gmail.com>
2024-03-28 01:17:55 +0530

    Fix line endings

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/signin' element={<SignIn/>}/>
+    <Route path='/signup' element={<SignUp/>}/>
+    <Route path='/projects' element={<Projects/>}/>
+    <Route path='/dashboard' element={<Dashboard/>}/>
+
+
+    </Routes>
+    
+    
+    
+    </BrowserRouter>
+  )
+}
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      aBOUT
+    </div>
+  )
+}
+
+export default About
